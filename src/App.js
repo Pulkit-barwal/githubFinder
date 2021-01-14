c915da0039de6ae4e26e6f9f362d05b252bad5da
Pulkit <pulkitbarwal@gmail.com>
2021-01-14 19:44:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Users from'./components/users/Users';
+import User from'./components/users/User';
+import Navbar from './components/layout/Navbar';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About'
+
+
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  
+  
+  
+
+ 
+  
+
+
+    return ( 
+      <GithubState>
+        <AlertState>
+
+        
+      <Router>
+      <div className='App'>
+       <Navbar icon='fab fa-github'></Navbar>
+       <div className="container">
+         <Alert /> 
+         <Switch>
+           <Route exact path='/' render={props => (
+             <Fragment>
+              <Search 
+              
+              />
+              <Users ></Users>
+             </Fragment>
+           )} >
+
+           </Route>
+
+           <Route exact path='/about' component={About} />
+           <Route exact path ='/user/:login'component={User} />
+         </Switch>
+       
+       </div>
+       
+      </div>
+      </Router>
+      </AlertState>
+      </GithubState>
+    );
+  
+  
 }
 
 export default App;
